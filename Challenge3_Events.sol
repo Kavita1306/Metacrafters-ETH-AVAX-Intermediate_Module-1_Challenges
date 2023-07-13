521b9f1968f80a8a9783c2e27a9ebe96c1d88405
Kavita1306 <121939699+Kavita1306@users.noreply.github.com>
2023-07-13 15:10:59 +0530

    Update Challenge3_Events.sol

--- Challenge3_Events.sol
+++ Challenge3_Events.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract EventExample {
+
+  event NewUser(address indexed _user);
+  event Deposit(address indexed _user, uint256 _amount);
+  event Withdrawal(address indexed _user, uint256 _amount);
+
+  constructor() {
+    emit NewUser(msg.sender);
+  }
+
+  function deposit(uint256 _amount) public {
+    emit Deposit(msg.sender, _amount);
+  }
+
+  function withdraw(uint256 _amount) public {
+    emit Withdrawal(msg.sender, _amount);
+  }
+
+}
